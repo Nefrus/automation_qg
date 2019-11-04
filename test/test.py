6d216eb56696f75ee29afd997ae20a21c2954ec2
zhuzhenyuan <zhenyuanzhu@outlook.com>
2019-11-04 15:44:37 +0800

    定时自动化

--- test/test.py
+++ test/test.py
@@ -1,7 +1,7 @@
 import uiautomator2 as u2
 
 
-def test_connect():
+def connect():
     # d = u2.connect("4fd2a842")  # connect to device
     d = u2.connect()  # connect to device 一台设备有线连接时，默认
     print(d.info)
@@ -9,4 +9,4 @@ def test_connect():
 
 
 if __name__ == "__main__":
-    test_connect()
+    connect()
